bf1db89123642d4eb8312bef59ea38a75850e423
JonaiLive <jonaisoto.dev@gmail.com>
2021-09-08 19:53:59 -0400

    learning with methods

--- methods/say.rb
+++ methods/say.rb
@@ -1,16 +1,9 @@
-def say(words)
-  puts words
+def say(words="type a word")
+  puts words + "."
 end
 
-
+say "dkgjdklgjkldf"
 say("hello")
 say("hi")
 say("how are you")
 say("I'm fine")
-
-
-
-puts "hello"
-puts "hi"
-puts "how are you"
-puts "I'm fine"
