1a731386274518d71bed1d71dd5efbea5d5de654
Jonai Soto <jonailive@gmail.com>
2021-09-11 15:05:08 -0400

    loops for the win

---
+++ loops/conditionalloop.rb
@@ -0,0 +1,22 @@
+# i = 0
+
+# loop do
+#     i = i + 2
+#     puts i
+#     if i == 10
+#         break
+#     end
+# end
+
+
+i = 0
+loop do
+    i = i + 2
+    if i == 4
+        next
+    end
+    puts i
+    if i == 10
+        break
+    end
+end
---
+++ loops/loopscopeexample.rb
@@ -0,0 +1,11 @@
+
+x = 42
+
+
+loop do
+    puts x
+    x = 2
+    break
+end
+
+puts x
