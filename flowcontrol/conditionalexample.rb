8da72cce42fe7c238556d60ae6bfac1103b347de
JonaiLive <jonaisoto.dev@gmail.com>
2021-09-11 10:10:47 -0400

    learning conditionals

---
+++ flowcontrol/conditionalexample.rb
@@ -0,0 +1,17 @@
+if x == 3
+  puts "x is 3"
+end
+
+if x == 3
+  puts " x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+if x == 3 then puts "x is 3" end
