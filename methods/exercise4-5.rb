13329bd8cca72b9a4cb72522c2b8da9c9f51631c
JonaiLive <jonaisoto.dev@gmail.com>
2021-09-10 22:29:51 -0400

    exercises

---
+++ methods/exercise4-5.rb
@@ -0,0 +1,14 @@
+# def scream(words)
+#   words = words + "!!!!!"
+#   return
+#   puts words
+# end
+
+# scream("jonai")
+
+def scream(words)
+  words = words + "!!!!!"
+  puts words
+end
+
+scream("jonai")
---
+++ methods/exercise6.rb
@@ -0,0 +1,7 @@
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+
+
+  This error tells me that the method invocation was expecting 2 arguments but received only 1.
