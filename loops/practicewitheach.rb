0b2aa983973c03cd4bef69aff0213e9baae130d1
Jonai Soto <jonailive@gmail.com>
2021-09-14 18:51:23 -0400

    playing with loops

---
+++ loops/practicewitheach.rb
@@ -0,0 +1,11 @@
+names = ["jonai", "joie", "david", "juan", "ada"]
+
+
+# names.each {|name| puts name.capitalize}
+
+
+def caps(names)
+    names.each {|name| puts name.capitalize}
+end
+
+caps(names)
