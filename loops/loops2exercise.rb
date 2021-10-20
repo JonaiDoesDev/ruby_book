bb4f69c995655283cbf828b4d8cd9adff783c2a5
JonaiLive <jonaisoto.dev@gmail.com>
2021-10-20 11:26:09 -0400

    loops2exercise

--- loops/loops2exercise.rb
+++ loops/loops2exercise.rb
@@ -9,13 +9,14 @@
 
 
 
-numbers = []
+puts numbers = []
+
 
 loop do
   puts 'Enter any number:'
   input = gets.chomp.to_i
-  numbers.push
-  break if numbers == 3 
+  numbers.push(input)
+  break if numbers.size == 5
   end
 
 puts numbers
