f905d894d69eec68833867afad48b83f21fd9a83
JonaiLive <jonaisoto.dev@gmail.com>
2021-10-26 14:34:07 -0400

    print something - user input

--- userinput/printsomething2.rb
+++ userinput/printsomething2.rb
@@ -1,11 +1,24 @@
-input = nil
+# input = nil
+
+# loop do 
+#   puts "Do you want me to print something? (y/n)"
+#   input = gets.chomp.downcase
+#   break if %w(y n and f love).include?(input)
+#   puts "Error thrown! Please enter y or n!"
+# end
+
+# puts "something" if input == "y"
+
+
+
+challenge = nil
+
 
 loop do 
   puts "Do you want me to print something? (y/n)"
-  input = gets.chomp.downcase
-  break if %w(y n).include?(input)
+  challenge = gets.chomp.downcase
+  break if %w(y n and f love).include?(challenge)
   puts "Error thrown! Please enter y or n!"
 end
 
-puts "something" if input == "y"
-
+puts "you got it" if challenge == "y"
