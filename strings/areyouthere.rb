7a5d26626073a5c512c5f4a050b02d96a2c7b1c7
Jonai Soto <jonailive@gmail.com>
2022-02-07 20:01:25 -0500

    arrayssssss

---
+++ strings/areyouthere.rb
@@ -0,0 +1,15 @@
+colors = 'blue pink yellow orange'
+
+if colors.include?('yellow')
+    print "True"
+else
+    print "False"
+end
+
+if colors.include?('purple')
+    print "True"
+else
+    print "False"
+end
+
+
