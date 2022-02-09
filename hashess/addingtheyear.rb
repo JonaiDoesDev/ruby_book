c6ee60e3119c7202d7ea7dba94eb976edb8c4478
JonaiLive <jonaisoto.dev@gmail.com>
2022-02-09 18:29:35 -0500

    hashess

---
+++ hashess/addingtheyear.rb
@@ -0,0 +1,10 @@
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000,
+  year: 2023
+}
+
+car[:year2] = 1999
+
+puts car
---
+++ hashess/firstcar.rb
@@ -0,0 +1,16 @@
+# car = {"type" => "sedan", "color" => "blue", "mileage" => 80_000}
+
+# car = {:type => "sedan", :color => "blue", :mileage => 80,000}
+
+car = {
+  type: 'sedan',
+  color: 'blue',
+  mileage: 80_000
+}
+
+
+
+p car
+
+
+
